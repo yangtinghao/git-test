1c430d854960818dec9906635ba417933e14cbf8
yth <1259789870@qq.com>
2025-11-04 21:47:55 +0800

    commit2

--- src/main/java/com/git/gitTest.java
+++ src/main/java/com/git/gitTest.java
@@ -2,6 +2,6 @@ package com.git;
 
 public class gitTest {
     public static void main(String[] args) {
-        System.out.println("hello git1");
+        System.out.println("hello git2");
     }
 }
