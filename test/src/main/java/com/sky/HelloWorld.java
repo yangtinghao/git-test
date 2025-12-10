8e48bd9e6a3347dc731bc0e1c6b49673b6fa5791
yangth <1259789870@qq.com>
2025-12-10 10:33:55 +0800

    Add new print statement to HelloWorld class

--- test/src/main/java/com/sky/HelloWorld.java
+++ test/src/main/java/com/sky/HelloWorld.java
@@ -2,6 +2,7 @@ package com.sky;
 
 public class HelloWorld {
      public static void main(String[] args) {
+        System.out.println("Hello World!333");
         System.out.println("Hello World!222");
         System.out.println("Hello World!111");
     }
