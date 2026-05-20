d86b48abe951ee8173c75c16d57acd9cb2fa19eb
yth <1259789870@qq.com>
2026-05-20 23:35:32 +0800

    1

--- test1/src/main/java/com/itheima/testDemp.java
+++ test1/src/main/java/com/itheima/testDemp.java
@@ -2,6 +2,6 @@ package com.itheima;
 
 public class testDemp {
     public static void main(String[] args) {
-        System.out.println("commit-seventh");
+        System.out.println("1");
     }
 }
