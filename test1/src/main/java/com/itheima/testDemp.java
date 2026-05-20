c4842e4779ddf7af1f988f3d9435d2d36c4aab48
yth <1259789870@qq.com>
2026-05-20 23:04:33 +0800

    commit-first

--- test1/src/main/java/com/itheima/testDemp.java
+++ test1/src/main/java/com/itheima/testDemp.java
@@ -2,6 +2,6 @@ package com.itheima;
 
 public class testDemp {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("commit-first");
     }
 }
